34c782a1158c807f88490481265005d994650cba
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2023-11-01 14:30:10 +0000

    add paths

--- hardhatTutorial/hardhat.config.js
+++ hardhatTutorial/hardhat.config.js
@@ -7,19 +7,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   //console.log(accounts[0]) ;
   for (const account of accounts) {
     const address=await account.address;
-    const balence=await account.balence;
+    const balence=await account.eth_getBalance;
     console.log(address," : " , balence);
   }
 });
 
 module.exports = {
   solidity: "0.8.19",
-  defaultNetwork:"sepolia",
+  defaultNetwork:"hardhat",
   networks:{
     hardhat:{},
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/VPbe3eoKGNUoC9mCcYF-H2tVO3GxK8im",
       accounts: ["9c77adff925b44b87fa38f297cfbdb19798da6bf1b0c0292f83caf668b92b15c"]
     }
-  }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
 };
