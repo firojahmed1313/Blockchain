3fd0117cd579a4f51fbc37cf386c21cd944a40a9
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2023-11-05 14:29:45 +0000

    test

--- hardhatTutorial/test/Lock.js
+++ hardhatTutorial/test/Lock.js
@@ -4,11 +4,17 @@ const {
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const {ethers} = require("hardhat");
+
 
 describe("Lotary", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  
-  
+  test('',async()=>{
+      const Lotary = await ethers.getContractFactory("lotary");
+      const lotary= await Lotary.deploy();
+      await lotary.deployed();
+  })
+
 });
