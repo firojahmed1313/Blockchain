2fd59160ad70cd358093467b2078b5ca6a883cb3
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-02-01 14:19:15 +0000

    funding setup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Blockchain
 
+
+```shell
+npm install --save-dev hardhat
+npx hardhat init
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+
---
+++ funding/contracts/funding.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract funding {
+    struct Data {
+        string name;
+        string message;
+        uint256 timestamp;
+        address from;
+    }
+
+    Data[] AllData;
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function sendMoney(string memory name, string memory message) public payable {
+        require(msg.value > 0, "Please pay greater than 0 ether");
+        owner.transfer(msg.value);
+        AllData.push(Data(name, message, block.timestamp, msg.sender));
+    }
+
+    function getData() public view returns (Data[] memory) {
+        return AllData;
+    }
+}
