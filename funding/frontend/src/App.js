b3c9f431b98a5c3d05fed0a12f3702712724c03e
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-02-03 14:55:17 +0000

    frontend setup

---
+++ funding/frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import abi from "./contact/funding.json"
+function App() {
+  console.log(abi);
+  return (
+    <>
+      <h1>firoj</h1>
+    </>
+  );
+}
+
+export default App;
